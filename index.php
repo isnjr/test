d9fde1ded06845e0ae328f262c8df711b581ebac
isnjr <65190506+isnjr@users.noreply.github.com>
2020-06-20 23:57:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- $sitelink="https://profildl.herokuapp.com/"; ?>
+ $sitelink="https://amrdown.000webhostapp.com/Instagram/Profile/"; ?>
 <!doctype html>
 <html lang="tr">
 <head>
@@ -56,7 +56,7 @@
 <body>
   <!-- Bu Script "Ökkeş Aksoy | Aksoyhlc" Tarafından Yapılmıştır -->
   <?php 
-  $sitelink="https://profildl.herokuapp.com/";
+  $sitelink="https://amrdown.000webhostapp.com/Instagram/Profile/";
   if (isset($_GET['username'])) {
    $username=$_GET['username']; ?>
 
